5016a46d90f1484f34d4236ce638598c09aa278b
Badal Golwalker <badalgolwalkar202@gmail.com>
2023-11-05 09:51:49 +0530

    latest

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ var multer = require("multer")
 passport.use(new localStrategy(usermodel.authenticate()))
 var mongoose = require("mongoose");
 const { stringify } = require('querystring');
+
 mongoose.connect("mongodb://0.0.0.0/spt-n15").then(() => {
   console.log("user connectd successfuly")
 }).catch(err => {
@@ -31,46 +32,6 @@ conn.once('open', () => {
   })
 })
 /* GET home page. */
-router.get("/", islogedIn, async function (req, res, next) {
-  const currentUser = await usermodel.findOne({_id:req.user._id})
-.populate("playlist").populate({
-  path:"playlist",
-  populate:{
-    path:"songs",
-    model:"song"
-  }
-})
-  res.render("index", { currentUser })
-})
-
-router.get('/poster/:postername',(req,res,next) =>{
-  gfsBucketPoster.openDownloadStreamByName(req.params.postername).pipe(res)
-})
-router.get("/stream/:musicname",async (req,res,next) =>{
-  const currentSong = await songmodel.findOne({filename:req.params.musicname})
- const stream = gfsBucket.openDownloadStreamByName(req.params.musicname)
-// console.log(currentSong)
- res.set('Content-Type','audio/mpeg')
-res.set('content-Length',currentSong.size +1)
-res.set('content-Range',`bytes 0-${currentSong.size - 1}/${currentSong.size}`)
-res.set('content-Ranges','bytes')
-res.status(206)
-
-stream.pipe(res)
-})
-router.get("/search",islogedIn,async (req,res,next)=>{
-let currentUser = await usermodel.findOne({username:req.session.passport.user})
-.populate("playlist")
-res.render("search",{currentUser})
-})
-router.post("/search",async (req,res,next) =>{
- const searchMusic =await songmodel.find({
-  title:{$regex:req.body.search}
- })
- res.json({
-  song:searchMusic
- })
-})
 router.post("/register", async function (req, res, next) {
   var newUSer = new usermodel({
     username: req.body.username,
@@ -86,9 +47,9 @@ router.post("/register", async function (req, res, next) {
           songs: songs.map(song => song._id)
         })
         let newUser = await usermodel.findOne({ _id: req.user._id })
- newUser.playlist.push(defaultPlaylist._id)
- await newUser.save()
-// console.log(JSON.stringify(currentUser))
+        newUser.playlist.push(defaultPlaylist._id)
+        await newUser.save()
+        // console.log(JSON.stringify(currentUser))
         res.redirect('/')
       })
     })
@@ -101,6 +62,55 @@ router.post('/login', passport.authenticate("local", {
 router.get("/login", function (req, res, next) {
   res.render("login")
 })
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/login');
+  });
+});
+
+router.get("/", islogedIn, async function (req, res, next) {
+  const currentUser = await usermodel.findOne({ _id: req.user._id })
+    .populate("playlist").populate({
+      path: "playlist",
+      populate: {
+        path: "songs",
+        model: "song"
+      }
+    })
+  res.render("index", { currentUser })
+})
+
+router.get('/poster/:postername', (req, res, next) => {
+  gfsBucketPoster.openDownloadStreamByName(req.params.postername).pipe(res)
+})
+router.get("/stream/:musicname", async (req, res, next) => {
+  const currentSong = await songmodel.findOne({ filename: req.params.musicname })
+  const stream = gfsBucket.openDownloadStreamByName(req.params.musicname)
+  // console.log(currentSong)
+  res.set('Content-Type', 'audio/mpeg')
+  res.set('content-Length', currentSong.size + 1)
+  res.set('content-Range', `bytes 0-${currentSong.size - 1}/${currentSong.size}`)
+  res.set('content-Ranges', 'bytes')
+  res.status(206)
+
+  stream.pipe(res)
+})
+router.get("/search", islogedIn, async (req, res, next) => {
+  let currentUser = await usermodel.findOne({ username: req.session.passport.user })
+    .populate("playlist")
+  res.render("search", { currentUser })
+})
+router.post("/search", async (req, res, next) => {
+  const searchMusic = await songmodel.find({
+    title: { $regex: req.body.search }
+  })
+  res.json({
+    song: searchMusic
+  })
+})
+
+
 //ye he data base  me upload karne ke liye memory storage
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
@@ -134,76 +144,70 @@ router.get("/uploadmusic", islogedIn, isAdmin, function (req, res, next) {
   res.render("uploadmusic")
 })
 
-router.post("/createplaylist",islogedIn,async (req,res,next) =>{
-let user =  await usermodel.findOne({username:req.session.passport.user})
+router.post("/createplaylist", islogedIn, async (req, res, next) => {
+  let user = await usermodel.findOne({ username: req.session.passport.user })
 
-if(user.playlist.length < 2 ){
-  let playlist = await playlistmodel.create({
-    name:req.body.filename,
-    owner:req.user._id
-  })
-  user.playlist.push(playlist._id)
-  user.save()
-  res.redirect("back")
-}
-
-
-})
-router.get('/save/:id',islogedIn, async (req,res,next)=>{
- let user = await usermodel.findOne({username:req.session.passport.user})
- let song = await songmodel.findOne({_id:req.params.id})
- if(user.favorateSong.indexOf(song._id) === -1){
-  user.favorateSong.push(song._id)
- }else{
-  user.favorateSong.splice(user.favorateSong.indexOf(song._id) , 1)
- }
- await user.save()
- res.redirect("back")
-
-})
-router.get('/logout', function (req, res, next) {
-  req.logout(function (err) {
-    if (err) { return next(err); }
-    res.redirect('/login');
-  });
-});
-
-router.get("/like/:id",islogedIn,(req,res,next) =>{
-usermodel.findOne({username:req.session.passport.user})
-.then(function(user){
-  songmodel.findOne({_id:req.params.id})
-  .then(function(song){
-    if(song.likes.indexOf(user._id) && user.liked.indexOf(song._id) === -1 ){
-      song.likes.push(user._id)
-      user.liked.push(song._id)
-    }
-    else{
-      song.likes.splice(song.likes.indexOf(user._id),1)
-     user.liked.splice(user.liked.indexOf(song._id),1)
-    }
-console.log(song.likes)
-console.log(user.liked)
+  if (user.playlist.length < 2) {
+    let playlist = await playlistmodel.create({
+      name: req.body.filename,
+      owner: req.user._id
+    })
+    user.playlist.push(playlist._id)
     user.save()
-    song.save()
-   .then(function(){
     res.redirect("back")
-   })
-  })
-})
-})
-router.get("/badal",islogedIn,async (req,res,next)=>{
-let user = await usermodel.findOne({username:req.session.passport.user})
-.populate("favorateSong")
-  res.render("badal",{user})
-})
-router.get("/delete/:id",islogedIn,async (req,res,next) =>{
-let user = await usermodel.findOne({username:req.session.passport.user})
-user.playlist = user.playlist.filter(playlist => playlist._id != req.params.id);
+  }
 
-let playlist = await playlistmodel.findOneAndDelete({_id:req.params.id})
-user.favorateSong = []
-await user.save()
-res.redirect("back")
+
+})
+router.get('/save/:id', islogedIn, async (req, res, next) => {
+  let user = await usermodel.findOne({ username: req.session.passport.user })
+  let song = await songmodel.findOne({ _id: req.params.id })
+  if (user.favorateSong.indexOf(song._id) === -1) {
+    user.favorateSong.push(song._id)
+  } else {
+    user.favorateSong.splice(user.favorateSong.indexOf(song._id), 1)
+  }
+  await user.save()
+  res.redirect("back")
+
+})
+
+router.get("/like/:id", islogedIn, (req, res, next) => {
+  usermodel.findOne({ username: req.session.passport.user })
+    .then(function (user) {
+      songmodel.findOne({ _id: req.params.id })
+        .then(function (song) {
+          if (song.likes.indexOf(user._id) && user.liked.indexOf(song._id) === -1) {
+            song.likes.push(user._id)
+            user.liked.push(song._id)
+          }
+          else {
+            song.likes.splice(song.likes.indexOf(user._id), 1)
+            user.liked.splice(user.liked.indexOf(song._id), 1)
+          }
+          console.log(song.likes)
+          console.log(user.liked)
+          user.save()
+          song.save()
+            .then(function () {
+              res.redirect("back")
+            })
+        })
+    })
+})
+router.get("/badal", islogedIn, async (req, res, next) => {
+  let user = await usermodel.findOne({ username: req.session.passport.user })
+    .populate("favorateSong")
+  res.render("badal", { user })
+})
+router.get("/delete/:id", islogedIn, async (req, res, next) => {
+  let user = await usermodel.findOne({ username: req.session.passport.user })
+  user.playlist = user.playlist.filter(playlist => playlist._id != req.params.id);
+
+  let playlist = await playlistmodel.findOneAndDelete({ _id: req.params.id })
+  user.favorateSong = []
+  await user.save()
+  res.redirect("back")
 })
 function islogedIn(req, res, next) {
   if (req.isAuthenticated()) {
